61afe35bdd679e503fc1dbb10b7f620aa4c22884
BishanPrabhashwara99 <bishanprabhashwara@gmail.com>
2025-11-22 14:58:45 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ const app = express();
 connectDB();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ['https://nyxae.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(morgan('combined'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
